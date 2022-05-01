552fb07299bf450e7f48cfe1e9b2957a2f814da5
QIU <24917456@qq.com>
2022-05-01 15:48:34 +0800

    hot-fix commit

--- src/main/java/com/qiu/gittest/GitTestApplication.java
+++ src/main/java/com/qiu/gittest/GitTestApplication.java
@@ -9,6 +9,8 @@ public class GitTestApplication {
     public static void main(String[] args) {
         SpringApplication.run(GitTestApplication.class, args);
         System.out.println("hello git");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
     }
 
 }
